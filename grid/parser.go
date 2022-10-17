013950f5c44d8f4f9fd5da15cc24eca50c643915
Björn Domars <bdomars@walkbase.com>
2022-10-17 12:01:32 +0300

    Add takeOne and parseLetter

--- grid/parser.go
+++ grid/parser.go
@@ -9,9 +9,9 @@ const (
 )
 
 type token struct {
-	data string
+	data byte
 	pos  int
-	kind
+	kind kind
 }
 
 type parser struct {
@@ -29,8 +29,52 @@ func newParser(input string) parser {
 	}
 }
 
+func (p *parser) takeOne() (token, error) {
+
+	if p.pos == len(p.data) {
+		return token{}, EndOfData{}
+	}
+
+	var kind kind
+	data := p.data[p.pos]
+	switch {
+	case data > 'A' && data < 'Z':
+		kind = LETTER
+	case data >= 0 && data <= 9:
+		kind = NUMBER
+	case data == '-' || data == '.' || data == ',':
+		kind = SEPARATOR
+	}
+
+	p.pos++
+
+	return token{
+		data: data,
+		kind: kind,
+		pos:  p.pos,
+	}, nil
+
+}
+
+func (p *parser) parseLetter() (byte, error) {
+	t, err := p.takeOne()
+	if err != nil {
+		return 0, err
+	}
+
+	if t.kind == LETTER {
+		return t.data, nil
+	}
+
+	return 0, SyntaxError{
+		pos:   t.pos,
+		token: t.data,
+	}
+}
+
 func (g Grid) Parse(input string) (GridRef, error) {
 	p := newParser(input)
 
+	p.result.Grid = &g
 	return p.result, nil
 }
