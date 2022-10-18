ea66f54ed42303e2e8396a91327a46285181bc11
Björn Domars <bdomars@walkbase.com>
2022-10-18 16:30:54 +0300

    Read stdin in loop

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -15,7 +16,6 @@ func main() {
 	flag.Parse()
 
 	base_str := flag.Arg(0)
-	target_str := flag.Arg(1)
 
 	g := grid.NewGrid(gridsize)
 
@@ -25,34 +25,45 @@ func main() {
 		os.Exit(1)
 	}
 
-	target, err := g.Parse(target_str)
-	if err != nil {
-		fmt.Printf("Failed to parse target coordinate: %v\n", err)
-		os.Exit(1)
-	}
-
 	base_pos := base.Position()
-	target_pos := target.Position()
 
-	flight_path := target_pos.Sub(base_pos)
+	cmdreader := bufio.NewReader(os.Stdin)
 
-	distance := grid.GetDistance(flight_path)
-	angle := grid.GetAngleDegrees(flight_path)
+	for {
+		fmt.Printf("> ")
+		target_str, err := cmdreader.ReadString('\n')
+		if err != nil {
+			os.Exit(0)
+		}
 
-	fmt.Printf("Base is %s\n", base)
-	fmt.Printf("Target is %s\n\n", target)
+		target, err := g.Parse(target_str)
+		if err != nil {
+			fmt.Printf("Failed to parse target coordinate: %v\n", err)
+			continue
+		}
 
-	fmt.Printf("Distance\t%6.1f m\n", distance)
+		target_pos := target.Position()
 
-	if distance > 1250 {
-		fmt.Printf("Target is out of range.\n")
+		flight_path := target_pos.Sub(base_pos)
+
+		distance := grid.GetDistance(flight_path)
+		angle := grid.GetAngleDegrees(flight_path)
+
+		fmt.Printf("Base is %s\n", base)
+		fmt.Printf("Target is %s\n\n", target)
+
+		fmt.Printf("Distance\t%6.1f m\n", distance)
+
+		if distance > 1250 {
+			fmt.Printf("Target is out of range.\n")
+		}
+
+		if distance < 50 {
+			fmt.Printf("Target is too close.\n")
+		}
+
+		fmt.Printf("Angle\t\t%6.1f °\n", angle)
+		fmt.Printf("Elevation\t%6.1f mil\n\n", grid.GetMils(distance))
 	}
-
-	if distance < 50 {
-		fmt.Printf("Target is too close.\n")
-	}
-
-	fmt.Printf("Angle\t\t%6.1f °\n", angle)
-	fmt.Printf("Elevation\t%6.1f mil\n\n", grid.GetMils(distance))
 
 }
